c8d03975bf12600da5e5462e04ed57f8d8ee1ebc
Brandon <railsdev14@gmail.com>
2016-03-02 13:24:33 -0500

    Added search by phone number

--- address_book.rb
+++ address_book.rb
@@ -7,6 +7,15 @@ class AddressBook
   def initialize
     @contacts = []
   end
+  def print_results(search, results)
+    puts search
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
+  end
 
   def find_by_name(name)
     results = []
@@ -15,14 +24,21 @@ class AddressBook
       if contact.full_name.downcase.include?(search)
         results.push(contact)
       end
-      puts "Name search results (#{search})"
-      results.each do |contact|
-        puts contact.to_s('full_name')
-        contact.print_phone_numbers
-        contact.print_addresses
-        puts "\n"
+    end
+    print_results("Name search results (#{search})", results)
+  end
+
+  def find_by_phone_number(number)
+    results = []
+    search = number.gsub("-", "")
+    contacts.each do |contact|
+      contact.phone_numbers.each do |phone_number|
+        if phone_number.number.gsub("-","").include?(search)
+          results.push(contact) unless results.include?(contact)
+        end
       end
     end
+    print_results("Phone Number search results (#{search})", results)
   end
 
   def print_contact_list
@@ -38,14 +54,14 @@ address_book = AddressBook.new
 jerry = Contact.new
 jerry.first_name = "Jerry"
 jerry.last_name = "Garcia"
-jerry.add_phone_number("Home", "234-234-2345")
+jerry.add_phone_number("Home", "234-234-0000")
 jerry.add_phone_number("Cell", "934-934-9345")
 jerry.add_address("Home", "400 Main St", "", "Chattanooga", "TN", "30740")
 
 jon = Contact.new
 jon.first_name = "Jon"
 jon.last_name = "Fishman"
-jon.add_phone_number("Home", "234-234-2345")
+jon.add_phone_number("Home", "555-234-2345")
 jon.add_phone_number("Cell", "934-934-9345")
 jon.add_address("Home", "400 Main St", "", "Burlington", "VT", "12345")
 
@@ -53,3 +69,4 @@ address_book.contacts.push(jerry)
 address_book.contacts.push(jon)
 
 address_book.find_by_name("i")
+address_book.find_by_phone_number("0")
